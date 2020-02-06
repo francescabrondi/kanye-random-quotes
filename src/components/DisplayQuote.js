0b49bd43d61befe261b6a46c2f5b57b616c342a1
Francesca <francescabrondi@gmail.com>
2020-02-06 21:47:36 +0100

    new

--- src/components/DisplayQuote.js
+++ src/components/DisplayQuote.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import "../style.css";
 
 class DisplayQuote extends React.Component {
   constructor(props) {
@@ -12,17 +13,25 @@ class DisplayQuote extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>{this.state.quote}</h2>
+        <div className="Title">
+          <h1>Random Kanye quote</h1>
+        </div>
+        <img
+          className="Image"
+          src="http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2566.png"
+          alt=""
+        />
         <button type="button" onClick={this.getQuote}>
-          Get Quote
+          Get Inspired
         </button>
+        <h2>{this.state.quote}</h2>
       </div>
     );
   }
 
   getQuote = () => {
     axios
-      .get("https://api.kanye.rest/")
+      .get("https://api.kanye.rest")
       .then(response => this.setState({ quote: response.data.quote }));
   };
 }
