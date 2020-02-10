b54b9bbc2f166d1ec076a6644d5f183abae3fac0
Francesca <francescabrondi@gmail.com>
2020-02-10 12:18:09 +0100

    added build

---
+++ build/precache-manifest.4a3cbe01cecf790820865e5b02fb4b90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac88f0aaca680d5681e3dfd09c380a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c0af449c085d43e1d33",
+    "url": "/static/css/main.6e520040.chunk.css"
+  },
+  {
+    "revision": "229374198abdd0e2d082",
+    "url": "/static/js/2.903294b7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.903294b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c0af449c085d43e1d33",
+    "url": "/static/js/main.d45c844d.chunk.js"
+  },
+  {
+    "revision": "9df57217bbd6c4b7ff91",
+    "url": "/static/js/runtime-main.0dbdb438.js"
+  }
+]);
