e2d331f2f0d14658c751320c06aafcfaf04a69d3
Francesca <francescabrondi@gmail.com>
2020-08-08 10:20:28 +0200

    image fixed

--- src/components/DisplayQuote.js
+++ src/components/DisplayQuote.js
@@ -3,37 +3,37 @@ import axios from "axios";
 import "../style.css";
 
 class DisplayQuote extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quote: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: "",
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="Title">
+                    <h1>Random Kanye quote</h1>
+                </div>
+                <img
+                    className="Image"
+                    src="../components/images/kanye2.png"
+                    alt=""
+                />
+                <button type="button" onClick={this.getQuote}>
+                    <p>Get Inspired</p>
+                </button>
+                <h2>{this.state.quote}</h2>
+            </div>
+        );
+    }
+
+    getQuote = () => {
+        axios
+            .get("https://api.kanye.rest")
+            .then((response) => this.setState({ quote: response.data.quote }));
     };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="Title">
-          <h1>Random Kanye quote</h1>
-        </div>
-        <img
-          className="Image"
-          src="http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2566.png"
-          alt=""
-        />
-        <button type="button" onClick={this.getQuote}>
-          <p>Get Inspired</p>
-        </button>
-        <h2>{this.state.quote}</h2>
-      </div>
-    );
-  }
-
-  getQuote = () => {
-    axios
-      .get("https://api.kanye.rest")
-      .then(response => this.setState({ quote: response.data.quote }));
-  };
 }
 
 export default DisplayQuote;
